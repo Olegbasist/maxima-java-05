235ebac2a77c8e0318d5adce1debe70ef0c488a7
Olegbasist <112417579+Olegbasist@users.noreply.github.com>
2023-01-22 18:22:01 +0700

    Удалил freemarker, перенастроил pom.xml

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,18 +1,17 @@
 package org.example;
 
-// TODO: Установить Tomcat
-//  Cделать генерацию страницы через обращение к сервлету и передачу ему параметров кота:
+// Установить Tomcat
+// TODO: Cделать генерацию страницы через обращение к сервлету и передачу ему параметров кота:
 //  String name, int weight, boolean isAngry
 
 
-import freemarker.template.TemplateException;
+
 
 import java.io.IOException;
 
 public class App
 {
-    public static void main( String[] args ) throws IOException, IncorrectCatWeightException, TemplateException {
-        DynamicPage dynamicPage = new DynamicPage();
-        dynamicPage.createPage("Cat");
+    public static void main( String[] args ) {
+        System.out.println("Let's make a cat!");
     }
 }
